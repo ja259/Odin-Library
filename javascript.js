2ddb4a532a437fb9a72d88c988bb2d87b2dc4633
James Otwane <127046977+ja259@users.noreply.github.com>
2023-06-10 10:11:33 -0400

    Add javascript.js

--- javascript.js
+++ javascript.js
@@ -1,301 +1,123 @@
-/* javascript */
+/* javascript.js */
 
+//button event listeners for create new book, add new book to page, close popup
+const addBtn = document.querySelector('#addBtn');
+addBtn.addEventListener('click', addBookToLibrary);
+
+const newBookBtn = document.querySelector('#newBtn');
+newBookBtn.addEventListener('click', () => popUpForm.style.display = 'block');
+
+const popUpForm = document.getElementById('popUp');
+const closePopUp = document.getElementsByTagName('span')[0];
+closePopUp.addEventListener('click', () => popUpForm.style.display = 'none');
+
+//Book Constructor
 class Book {
-  constructor(
-    title = 'Unknown',
-    author = 'Unknown',
-    pages = '0',
-    isRead = false
-  ) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.isRead = isRead
-  }
-}
-class Library {
-  constructor() {
-    this.books = []
-  }
-  addBook(newBook) {
-    if (!this.isInLibrary(newBook)) {
-      this.books.push(newBook)
+    constructor(title, author, pages, read) {
+        this.title = form.title.value; 
+        this.author = form.author.value; 
+        this.pages = form.pages.value + 'pg'; 
+        this.read = form.read.checked; 
     }
-  }
-  removeBook(title) {
-    this.books = this.books.filter((book) => book.title !== title)
-  }
-  getBook(title) {
-    return this.books.find((book) => book.title === title)
-  }
-  isInLibrary(newBook) {
-    return this.books.some((book) => book.title === newBook.title)
-  }
-}
-const library = new Library()
-// User Interface
-const accountBtn = document.getElementById('accountBtn')
-const accountModal = document.getElementById('accountModal')
-const addBookBtn = document.getElementById('addBookBtn')
-const addBookModal = document.getElementById('addBookModal')
-const errorMsg = document.getElementById('errorMsg')
-const overlay = document.getElementById('overlay')
-const addBookForm = document.getElementById('addBookForm')
-const booksGrid = document.getElementById('booksGrid')
-const loggedIn = document.getElementById('loggedIn')
-const loggedOut = document.getElementById('loggedOut')
-const loadingRing = document.getElementById('loadingRing')
-const setupNavbar = (user) => {
-  if (user) {
-    loggedIn.classList.add('active')
-    loggedOut.classList.remove('active')
-  } else {
-    loggedIn.classList.remove('active')
-    loggedOut.classList.add('active')
-  }
-  loadingRing.classList.remove('active')
-}
-const setupAccountModal = (user) => {
-  if (user) {
-    accountModal.innerHTML = `
-      <p>Logged in as</p>
-      <p><strong>${user.email.split('@')[0]}</strong></p>`
-  } else {
-    accountModal.innerHTML = ''
-  }
-}
-const openAddBookModal = () => {
-  addBookForm.reset()
-  addBookModal.classList.add('active')
-  overlay.classList.add('active')
-}
-const closeAddBookModal = () => {
-  addBookModal.classList.remove('active')
-  overlay.classList.remove('active')
-  errorMsg.classList.remove('active')
-  errorMsg.textContent = ''
-}
-const openAccountModal = () => {
-  accountModal.classList.add('active')
-  overlay.classList.add('active')
-}
-const closeAccountModal = () => {
-  accountModal.classList.remove('active')
-  overlay.classList.remove('active')
-}
-const closeAllModals = () => {
-  closeAddBookModal()
-  closeAccountModal()
-}
-const handleKeyboardInput = (e) => {
-  if (e.key === 'Escape') closeAllModals()
-}
-const updateBooksGrid = () => {
-  resetBooksGrid()
-  for (let book of library.books) {
-    createBookCard(book)
-  }
-}
-const resetBooksGrid = () => {
-  booksGrid.innerHTML = ''
 }
 
-const createBookCard = (book) => {
-  const bookCard = document.createElement('div')
-  const title = document.createElement('h3')
-  const author = document.createElement('h3')
-  const pages = document.createElement('h3')
-  const title = document.createElement('p')
-  const author = document.createElement('p')
-  const pages = document.createElement('p')
-  const buttonGroup = document.createElement('div')
-  const readBtn = document.createElement('button')
-  const removeBtn = document.createElement('button')
+//creates book from Book Constructor, adds to library
+let myLibrary = [];
+let newBook;
 
-  bookCard.classList.add('book-card')
-  buttonGroup.classList.add('button-group')
-  readBtn.classList.add('btn')
-  removeBtn.classList.add('btn')
-  removeBtn.classList.add('btn-red')
-  readBtn.onclick = toggleRead
-  removeBtn.onclick = removeBook
+function addBookToLibrary() {
+    event.preventDefault();
+    popUpForm.style.display = 'none';
 
-  title.textContent = `"${book.title}"`
-  author.textContent = book.author
-  pages.textContent = `${book.pages} pages`
-  removeBtn.textContent = 'Remove'
-  if (book.isRead) {
-    readBtn.textContent = 'Read'
-    readBtn.classList.add('btn-light-green')
-  } else {
-    readBtn.textContent = 'Not read'
-    readBtn.classList.add('btn-light-red')
-  }
-  bookCard.appendChild(title)
-  bookCard.appendChild(author)
-  bookCard.appendChild(pages)
-  bookCard.appendChild(readBtn)
-  bookCard.appendChild(removeBtn)
-  buttonGroup.appendChild(readBtn)
-  buttonGroup.appendChild(removeBtn)
-  bookCard.appendChild(buttonGroup)
-  booksGrid.appendChild(bookCard)
+    newBook = new Book(title, author, pages,read); 
+    myLibrary.push(newBook); 
+    setData();  //saves updated array in local storage
+    render(); 
+    form.reset();
 }
 
-const getBookFromInput = () => {
-  const title = document.getElementById('title').value
-  const author = document.getElementById('author').value
-  const pages = document.getElementById('pages').value
-  const isRead = document.getElementById('isRead').checked
-  return new Book(title, author, pages, isRead)
-}
-const addBook = (e) => {
-  e.preventDefault()
-  const newBook = getBookFromInput()
-  if (library.isInLibrary(newBook)) {
-    errorMsg.textContent = 'This book already exists in your library'
-    errorMsg.classList.add('active')
-    return
-  }
-  if (auth.currentUser) {
-    addBookDB(newBook)
-  } else {
-    library.addBook(newBook)
-    saveLocal()
-    updateBooksGrid()
-  }
-  closeAddBookModal()
+//Creates book visual in browser
+function render() {
+    const display = document.getElementById('Library-container');
+    const books = document.querySelectorAll('.book');
+    books.forEach(book => display.removeChild(book));
+   
+    for (let i=0; i<myLibrary.length; i++){
+        createBook(myLibrary[i]);
+    }
 }
 
-const removeBook = (e) => {
-  const title = e.target.parentNode.firstChild.innerHTML.replaceAll('"', '')
-  const title = e.target.parentNode.parentNode.firstChild.innerHTML.replaceAll(
-    '"',
-    ''
-  )
+//creats book DOM elements, to use in render();
+function createBook(item) {
+    const library = document.querySelector('#Library-container');
+    const bookDiv = document.createElement('div');
+    const titleDiv = document.createElement('div');
+    const authDiv = document.createElement('div');
+    const pageDiv = document.createElement('div');
+    const removeBtn = document.createElement('button');
+    const readBtn = document.createElement('button');
+    
+    
+    bookDiv.classList.add('book');
+    bookDiv.setAttribute('id', myLibrary.indexOf(item));
 
-  if (auth.currentUser) {
-    removeBookDB(title)
-  } else {
-    library.removeBook(title)
-    saveLocal()
-    updateBooksGrid()
-  }
+    titleDiv.textContent = item.title;
+    titleDiv.classList.add('title');
+    bookDiv.appendChild(titleDiv);
+
+    authDiv.textContent = item.author;
+    authDiv.classList.add('author');
+    bookDiv.appendChild(authDiv);
+
+    pageDiv.textContent = item.pages;
+    pageDiv.classList.add('pages');
+    bookDiv.appendChild(pageDiv);
+
+    readBtn.classList.add('readBtn')    
+    bookDiv.appendChild(readBtn);
+    if(item.read===false) {
+        readBtn.textContent = 'Not Read';
+        readBtn.style.backgroundColor = '#e04f63';
+    }else {
+        readBtn.textContent = 'Read';
+        readBtn.style.backgroundColor = '#63da63'
+    }
+
+    removeBtn.textContent = 'Remove'; 
+    removeBtn.setAttribute('id', 'removeBtn');
+    bookDiv.appendChild(removeBtn);
+    
+    library.appendChild(bookDiv);
+
+    removeBtn.addEventListener('click', () => {
+        myLibrary.splice(myLibrary.indexOf(item),1);
+        setData()
+        render();
+    });
+
+    //add toggle ability to each book 'read' button on click
+    readBtn.addEventListener('click', () => { 
+        item.read = !item.read; 
+        setData(); 
+        render();
+    }); 
+};
+
+// setting Library to be stored in local storage
+function setData() {
+    localStorage.setItem(`myLibrary`, JSON.stringify(myLibrary));
 }
 
-const toggleRead = (e) => {
-  const title = e.target.parentNode.firstChild.innerHTML.replaceAll('"', '')
-  const title = e.target.parentNode.parentNode.firstChild.innerHTML.replaceAll(
-    '"',
-    ''
-  )
-  const book = library.getBook(title)
+//pulls books from local storage when page is refreshed
+function restore() {
+    if(!localStorage.myLibrary) {
+        render();
+    }else {
+        let objects = localStorage.getItem('myLibrary') // gets information from local storage to use in below loop to create DOM/display
+        objects = JSON.parse(objects);
+        myLibrary = objects;
+        render();
+    }
+}
 
-  if (auth.currentUser) {
-    toggleBookIsReadDB(book)
-  } else {
-    book.isRead = !book.isRead
-    saveLocal()
-    updateBooksGrid()
-  }
-}
-accountBtn.onclick = openAccountModal
-addBookBtn.onclick = openAddBookModal
-overlay.onclick = closeAllModals
-addBookForm.onsubmit = addBook
-window.onkeydown = handleKeyboardInput
-// Local Storage
-const saveLocal = () => {
-  localStorage.setItem('library', JSON.stringify(library.books))
-}
-const restoreLocal = () => {
-  const books = JSON.parse(localStorage.getItem('library'))
-  if (books) {
-    library.books = books.map((book) => JSONToBook(book))
-  } else {
-    library.books = []
-  }
-}
-// Auth
-const auth = firebase.auth()
-const logInBtn = document.getElementById('logInBtn')
-const logOutBtn = document.getElementById('logOutBtn')
-auth.onAuthStateChanged(async (user) => {
-  if (user) {
-    setupRealTimeListener()
-  } else {
-    if (unsubscribe) unsubscribe()
-    restoreLocal()
-    updateBooksGrid()
-  }
-  setupAccountModal(user)
-  setupNavbar(user)
-})
-const signIn = () => {
-  const provider = new firebase.auth.GoogleAuthProvider()
-  auth.signInWithPopup(provider)
-}
-const signOut = () => {
-  auth.signOut()
-}
-logInBtn.onclick = signIn
-logOutBtn.onclick = signOut
-// Firestore
-const db = firebase.firestore()
-let unsubscribe
-const setupRealTimeListener = () => {
-  unsubscribe = db
-    .collection('books')
-    .where('ownerId', '==', auth.currentUser.uid)
-    .orderBy('createdAt')
-    .onSnapshot((snapshot) => {
-      library.books = docsToBooks(snapshot.docs)
-      updateBooksGrid()
-    })
-}
-const addBookDB = (newBook) => {
-  db.collection('books').add(bookToDoc(newBook))
-}
-const removeBookDB = async (title) => {
-  db.collection('books')
-    .doc(await getBookIdDB(title))
-    .delete()
-}
-const toggleBookIsReadDB = async (book) => {
-  db.collection('books')
-    .doc(await getBookIdDB(book.title))
-    .update({ isRead: !book.isRead })
-}
-const getBookIdDB = async (title) => {
-  const snapshot = await db
-    .collection('books')
-    .where('ownerId', '==', auth.currentUser.uid)
-    .where('title', '==', title)
-    .get()
-  const bookId = snapshot.docs.map((doc) => doc.id).join('')
-  return bookId
-}
-// Utils
-const docsToBooks = (docs) => {
-  return docs.map((doc) => {
-    return new Book(
-      doc.data().title,
-      doc.data().author,
-      doc.data().pages,
-      doc.data().isRead
-    )
-  })
-}
-const JSONToBook = (book) => {
-  return new Book(book.title, book.author, book.pages, book.isRead)
-}
-const bookToDoc = (book) => {
-  return {
-    ownerId: auth.currentUser.uid,
-    title: book.title,
-    author: book.author,
-    pages: book.pages,
-    isRead: book.isRead,
-    createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-  }
-}
+restore();
